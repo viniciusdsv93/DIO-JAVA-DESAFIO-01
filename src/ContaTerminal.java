78f909ef8da59cdebc666d90dd089adaaf125ff6
Vinícius Verissimo <86579020+viniciusdsv93@users.noreply.github.com>
2022-07-14 08:40:21 -0300

    correct questions language

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -5,16 +5,16 @@ public class ContaTerminal {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in).useLocale(Locale.US);
 
-    System.out.println("Enter your account's number:");
+    System.out.println("Por favor, digite o número da conta:");
     int accountNumber = sc.nextInt();
 
-    System.out.println("Enter your account's agency:");
+    System.out.println("Por favor, digite o número da agência:");
     String accountAgency = sc.next();
 
-    System.out.println("Enter your account's number:");
+    System.out.println("Por favor, digite o nome do cliente:");
     String clientName = sc.next();
 
-    System.out.println("Enter your total amount:");
+    System.out.println("Por favor, digite o saldo:");
     double amount = sc.nextDouble();
 
     System.out.println("Olá " + clientName + ", obrigado por criar uma conta em nosso banco, sua agência é " + accountAgency + ", conta " + accountNumber + " e seu saldo " + amount + " já está disponível para saque");
